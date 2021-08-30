f8dcc1af7dec3ac8e5130f21d16116d9b3bddec6
admin <admin@DESKTOP-COIHK7E>
2021-08-31 07:21:53 +0900

    ex7no33

---
+++ ex7no33/src/ex7no33/ex7no33.java
@@ -0,0 +1,29 @@
+package ex7no33;
+
+public class ex7no33 {  //  407p
+	private int outerIv = 0;
+	static int outerCv = 0;
+	
+	class InstanceInner2 {
+		int ilv = outerIv;
+		int scv = outerCv;
+	}
+	
+	static class StaticInner2 {
+//		int siv = outerIv;
+		static int scv = outerCv;
+	}
+	
+	void Method() {
+		int lv = 0;
+		final int LV = 0;
+		
+		class LocalInner2 {
+			int liv = outerIv;
+			int liv2 = outerCv;
+			
+			int liv3 = lv;
+			int liv4 = LV;
+		}
+	}
+}
